6e0636cf64a080ae4f2f45857d096c426f25b86c
Derek Johnston <derek@epidemico.com>
2015-10-16 14:55:34 -0400

    skelton sidebar

--- Kivu/client/components/layout/Header.import.jsx
+++ Kivu/client/components/layout/Header.import.jsx
@@ -1,9 +1,45 @@
-var {AppBar} = MUI;
+var {AppBar, LeftNav, IconMenu, MenuItem, IconButton, Avatar} = MUI;
 
 Header = React.createClass({
+
+  _toggleSideBar(e) {
+    return this.refs.leftNav.toggle();
+  },
+
   render() {
+    console.log(MenuItem.Types);
+    var menuItems = [{
+      type: MenuItem.Types.SUBHEADER,
+      text: "My Diary"
+    },
+    {
+      text: "Create New Entry"
+    }, {
+      text: "Analyze Diary"
+    }, {
+      type: MenuItem.Types.SUBHEADER,
+      text: "My Profile"
+    }, {
+      text: "Edit Profile"
+    }, {
+      text: "Logout"
+    }, {
+      type: MenuItem.Types.SUBHEADER,
+      text: "Help"
+    }, {
+      text: "Getting Started"
+    }, {
+      text: "FAQs"
+    }, {
+      text: "Contact Us"
+    }];
+
     return (
-      <AppBar title="Kivu" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+      <div>
+        <AppBar title="Title Your App" onLeftIconButtonTouchTap={this._toggleSideBar}>
+        </AppBar>
+        <LeftNav ref="leftNav" menuItems={menuItems} docked={false}></LeftNav>
+      </div>
     );
   }
 });
--- Kivu/client/components/layout/MainLayout.import.jsx
+++ Kivu/client/components/layout/MainLayout.import.jsx
@@ -2,15 +2,18 @@ injectTapEventPlugin();
 
 var {AppCanvas, Styles} = MUI;
 var {ThemeManager, LightRawTheme} = Styles;
+var {Grid} = ReactBootstrap;
 
 MainLayout = React.createClass({
 
   render() {
     return (
       <AppCanvas>
-        {this.props.header}
-        {this.props.content}
-        {this.props.footer}
+        <Grid>
+          {this.props.header}
+          {this.props.content}
+          {this.props.footer}
+        </Grid>
       </AppCanvas>
     );
   }
