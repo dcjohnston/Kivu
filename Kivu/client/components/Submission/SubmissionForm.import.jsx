c832f732b1a297cf839aba979fd529a5019534a0
Derek Johnston <derek@epidemico.com>
2015-10-17 14:04:50 -0400

    add fields to submission form

--- Kivu/client/components/Submission/SubmissionForm.import.jsx
+++ Kivu/client/components/Submission/SubmissionForm.import.jsx
@@ -1,23 +1,33 @@
-var {Slider, Paper, DatePicker} = MUI;
+import SliderWrapper from '/client/components/Submission/SliderWrapper';
+import HumanModel from '/client/components/dataVis/HumanModel';
+import PeriodTracker from '/client/components/Submission/Period';
+import AssistedDevices from '/client/components/Submission/AssistedDevices';
+import Exercise from '/client/components/Submission/Exercise';
+var {Paper} = MUI;
 
 SubmissionForm = React.createClass({
   render() {
 
     var lastPeriod;
     if (true) { //user is female
-      lastPeriod = <DatePicker mode="landscape"/>;
+      lastPeriod = <PeriodTracker/>;
     }
 
     return (
       <Paper zDepth={1} className="submission-container">
         <form>
-          <Slider name="pain" defaultValue={0} max={10} min={0}/>
-          <Slider name="energy" OveralldefaultValue={0} max={10} min={0}/>
-          <Slider name="mood" defaultValue={0} max={10} min={0}/>
-          <Slider name="stress" defaultValue={0} max={10} min={0}/>
-          <Slider name="anxiety" defaultValue={0} max={10} min={0}/>
+          <HumanModel />
+          <SliderWrapper name="pain" defaultValue={0} max={10}
+            min={0} prompt={"Pain"}/>
+          <SliderWrapper name="energy" OveralldefaultValue={0}
+            max={10} min={0} prompt={"Energy"}/>
+          <SliderWrapper name="mood" defaultValue={0}
+            max={10} min={0} prompt={"Mood"}/>
+          <SliderWrapper name="stress" defaultValue={0}
+            max={10} min={0} prompt={"Stress"}/>
           {lastPeriod}
-
+          <AssistedDevices></AssistedDevices>
+          <Exercise></Exercise>
         </form>
       </Paper>
     );
